72373b68523c83f49fb9598f0814ddd80e498096
mindwork64 <vmurashov85@gmail.com>
2017-12-07 21:47:54 +0300

    Added main source. Makefile in progress

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+	{
+		float uSrc; // Напряжение источника питания
+		int iLed; // Ток диода
+		float uLed; // Прямое напряжение диода
+				
+		cout << "Напряжение источника: ";
+		cin >> uSrc;
+		cout << "Напряжение диода: ";
+		cin >> uLed;
+		cout << "Ток диода: ";
+		cin >> iLed;
+				
+		float fResLed = (uSrc - uLed) / ((float)iLed / 1000);
+		float fResWatt = (pow(iLed, 2) * fResLed) / 10000;
+				
+		cout << "Вычисленное сопротивление резистора: " << "\x1b[32m" << fResLed << "\x1b[37mОм" << endl;
+		cout << "Вычисленная мощность резистора: " << "\x1b[32m" << fResWatt << "\x1b[37mмВт" << endl;
+	}
